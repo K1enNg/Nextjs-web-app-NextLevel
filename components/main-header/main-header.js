b27f251a6c4e458b702c60ef6f7c73d02edfabe3
K1enNg <hiimkien2702@gmail.com>
2024-12-09 18:04:33 -0500

    optimize cilent components

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -3,6 +3,7 @@ import logoImg from "@/assets/logo.png"
 import classes from "./main-header.module.css"
 import Image from "next/image";
 import MainHeaderBackground from "./main-header-background";
+import NavLink from "./nav-link";
 
 const MainHeader = (() => {
     return (
@@ -16,9 +17,12 @@ const MainHeader = (() => {
 
                 <nav className={classes.nav}>
                     <ul>
-                        <li><Link href="/meals">Browse Meal</Link></li>
-                        <li><Link href="/community">Community</Link></li>
-
+                        <li>
+                            <NavLink href='/meals'>Browse Meals</NavLink>
+                        </li>
+                        <li>
+                            <NavLink href='/community'>Foodies Community</NavLink>
+                        </li>
                     </ul>
                 </nav>
             </header>
